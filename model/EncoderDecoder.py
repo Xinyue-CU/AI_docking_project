bfda74e80c5bb4ee8026a7ce70ee21076c241ed8
shenxinyue <xs2523@columbia.edu>
2023-11-27 09:06:26 -0500

    Commit test1

---
+++ model/EncoderDecoder.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+
+
+class EncoderDecoder(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(EncoderDecoder, self).__init__()
+
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.encoder = nn.LSTM(hidden_size, hidden_size, batch_first=True)
+
+
+        self.decoder = nn.LSTM(hidden_size, hidden_size, batch_first=True)
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        x = self.embedding(x)
+        _, (hidden, cell) = self.encoder(x)
+
+        decoder_input = x[:, -1, :].unsqueeze(1)  # 使用编码器最后一个时间步的输出作为解码器的输入
+        decoder_output, _ = self.decoder(decoder_input, (hidden, cell))
+
+        output = self.fc(decoder_output[:, -1, :])  # 取解码器最后一个时间步的输出
+        return output
