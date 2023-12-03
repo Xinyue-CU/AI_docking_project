9dfecdd7ba74acd60a183907390b2a5268bb9d09
shenxinyue <xs2523@columbia.edu>
2023-12-03 18:36:35 -0500

    commit

--- Oversampling.py
+++ Oversampling.py
@@ -12,7 +12,7 @@ target_scores = [-11, -10, -9, -2, -1]
 resampled_data = pd.DataFrame()
 for score in target_scores:
     subset = df[df['docking score'].between(score - 0.5, score + 0.5)]
-    resampled_subset = subset.sample(n=2000, replace=True)  # your_resample_size是重采样的大小
+    resampled_subset = subset.sample(n=2000, replace=True)
     resampled_data = pd.concat([resampled_data, resampled_subset])
 
 
