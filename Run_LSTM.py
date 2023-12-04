6b4696988fae0014c08913ab93c39fcff2ff900b
shenxinyue <xs2523@columbia.edu>
2023-12-04 10:43:11 -0500

    fix the padding problem and the index problem

--- Run_LSTM.py
+++ Run_LSTM.py
@@ -1,5 +1,4 @@
 import codecs
-import matplotlib.pyplot as plt
 import torch
 from tqdm import tqdm
 import torch.nn as nn
@@ -9,10 +8,9 @@ import os
 from SmilesPE.learner import *
 from SmilesPE.tokenizer import *
 from SmilesPE.spe2vec import *
-from model.LSTM1 import LSTM_2layer
+from model.LSTM_model import *
 from model.data_preprocessing import padding, get_data
 from sklearn.metrics import r2_score
-
 
 # 1. Prepare the vocabulary
 dataset_name = 'resampled_data.csv'
@@ -43,7 +41,6 @@ model = learn_spe2vec(corpus=corpus, outfile=None, vector_size=100,
                       method='skip-gram')  # learn_spe2vec is a function in spe2vec.py in SmilesPE
 print('Build up spe2vec model successfully!')
 
-
 # Save the trained model
 model_path = 'spe_model.bin'
 model.save(model_path)
@@ -54,84 +51,81 @@ print('Get spe2vec model successfully!')
 df_smiles['tokenize'] = df_smiles['SMILES'].apply(lambda x: spe.tokenize(x))
 print('Get tokenize result successfully!')
 
+
 def process_embedding(smiles):
     embedding = spe2vec.smiles2vec(smiles)
     return embedding
 
+
 tqdm.pandas(desc="Processing SMILES embeddings")
 df_smiles['embedding'] = df_smiles['SMILES'].progress_apply(process_embedding)
 
-
 # save the result
+# to_csv
 df_smiles.to_csv('SAMPLE_SMILE1_embedding_result.csv', index=False)
 original_smiles = df_smiles['SMILES'].tolist()
 smiles2seq_list = df_smiles['embedding'].tolist()  # X
 docking_score = df_smiles['docking score'].tolist()  # y
 
-# save the embedding result to pickle file
+# to_pickle
 pickle_file_path = 'embedding_data.pkl'
-tqdm.write("Saving the embedding data...")
-df_smiles[['embedding', 'SMILES', 'docking score']].to_pickle(pickle_file_path)
+df_smiles.to_pickle(pickle_file_path)
 tqdm.write("Embedding data saved successfully!")
 
+# 4. Data preprocessing: load the embedding data
 tqdm.write("Loading the embedding data...")
-loaded_df = pd.read_pickle(pickle_file_path)
+df_smiles = pd.read_pickle(pickle_file_path)
 tqdm.write("Embedding data loaded successfully!")
+
+original_smiles = df_smiles['SMILES'].tolist()
+smiles2seq_list = df_smiles['embedding'].tolist()  # X in the model
+docking_score = df_smiles['docking score'].tolist()  # y in the model
+original_length = [len(seq) for seq in smiles2seq_list]
+
+# find one element with 0 length, can will cause error in the later training process
+# we need to remove it
+index_list = [i for i in range(len(original_length)) if original_length[i] == 0]
+smiles2seq_list = [element for index, element in enumerate(smiles2seq_list) if index not in index_list]
+docking_score = [element for index, element in enumerate(docking_score) if index not in index_list]
+original_length = [element for index, element in enumerate(original_length) if index not in index_list]
 print('Get the embedding result successfully！')
 
-
-# 4. Data preprocessing: padding and train test split
+# 5. Data preprocessing: padding and train test split
 max_seq_length, X_padding = padding(smiles2seq_list, num_features=100)
 X = np.array(X_padding)
 y = np.array(docking_score)
 train_loader, val_loader, test_loader, y_train_tensor, \
-y_val_tensor, y_test_tensor, smiles_train, smiles_val, smiles_test = get_data(original_smiles, X, y)
-print('Get the data successfully!')
+y_val_tensor, y_test_tensor, smiles_train, smiles_val, smiles_test = get_data(original_length, X, y)
+print('Prepare the data successfully!')
 
+# 6. Train the model
+input_dim = 100
+LSTM_hidden_size1 = 100
+LSTM_hidden_size2 = 100
+LSTM_output_dim = 64
+linear_hidden_size1 = 32
+linear_hidden_size2 = 16
+output_size = 1
+dropout_prob = 0.5
+num_epochs = 10
+learning_rate = 0.001
 
-# 5. Build up the model
-# input_size = 100
-# hidden_size1 = 64
-# hidden_size2 = 32
-# hidden_size3 = 16
-# num_layers = 2  # Number of LSTM layers
-# num_classes = 1  # Number of output classes
-# learning_rate = 0.01
-# num_epochs = 100  # Number of training epochs
-# seq_length = max_seq_length  # Length of the input sequence
-# dropout_prob = 0.2  # Dropout rate
-
-num_classes = 1
-input_size = 100
-hidden_size1 = 64
-hidden_size2 = 32
-num_layers = 1
-seq_length = max_seq_length
-dropout_prob = 0.2
-learning_rate = 0.01
-num_epochs = 100
-
-model = LSTM_2layer(num_classes, input_size, hidden_size1, hidden_size2, num_layers, seq_length, dropout_prob)
-
-# model = LSTM(num_classes, input_size, hidden_size1, hidden_size2, hidden_size3, num_layers, seq_length, dropout_prob)
+model = TwoLayerLSTMModel(input_dim, LSTM_hidden_size1, LSTM_hidden_size2, LSTM_output_dim, linear_hidden_size1,
+                          linear_hidden_size2, output_size, dropout_rate=0.5)
 criterion = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-
-# Training loop
-print('Start training.....')
 for epoch in range(num_epochs):
-    epoch_loss = 0.0
-    for inputs, labels in train_loader:
+    for inputs, labels, length in train_loader:
+        labels = labels.unsqueeze(1)
+        output = model(inputs, length)
+        loss = criterion(output, labels)
         optimizer.zero_grad()
-        outputs = model(inputs)
-        loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
-        epoch_loss += loss.item()
 
-    epoch_loss /= len(train_loader)
-    print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}')
+    # Print the loss every epoch
+    print(f"Epoch {epoch + 1}/{num_epochs}, Loss: {loss.item()}")
 
 # Save the model
 torch.save(model, 'LSTM_model.pth')
@@ -139,24 +133,21 @@ loaded_model = torch.load('LSTM_model.pth')
 loaded_model.eval()
 print('Training completed.')
 
-
 # 6. Evaluate the model
 model.eval()
 predictions = []
 with torch.no_grad():
-    for inputs, labels in test_loader:
-        outputs = model(inputs)
-        predictions.append(outputs.detach().cpu().numpy())
+    for inputs, labels, length in test_loader:
+        output = model(inputs, length)
+        predictions.append(output.detach().cpu().numpy())
 
 predictions = np.concatenate(predictions)
-print(predictions)
 r2 = r2_score(y_test_tensor, predictions)
 print(f'R-squared Score on Test Set: {r2:.4f}')
 
 # Save the result
-print(smiles_test[0:10])
 print(y_test_tensor.flatten()[0:10])
 print(predictions.flatten()[0:10])
-df_test = pd.DataFrame({'SMILES': smiles_test, 'docking score': y_test_tensor.flatten(), 'predictions': predictions.flatten()})
+df_test = pd.DataFrame(
+    {'SMILES': smiles_test, 'docking score': y_test_tensor.flatten(), 'predictions': predictions.flatten()})
 df_test.to_csv('Test_result.csv', index=False)
-
